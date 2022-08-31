0b87158343d1377dcb15b137518c3edbfbe18f37
DylanLebel <Ontarionorthland1000@gmail.com>
2022-08-30 21:46:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Ready!BOY');
 });
 
 client.on('interactionCreate', async interaction => {
